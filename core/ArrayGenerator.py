d4cdfde3093da3530f72b30cfea665467af87dc5
sheunl <sheunvi@gmail.com>
2022-11-20 17:55:28 -0500

    Array Generator Completed

--- core/ArrayGenerator.py
+++ core/ArrayGenerator.py
@@ -12,13 +12,14 @@ class ArrayGenerator(ClassicGenerator):
     def __init__(self,length=10, type='Integer',low=0,high=9) -> None:
         __types={'AlphaNumeric':1,'Alphabet':2,'Alphabet_Upper':3,'Alphabet_Lower':4, 'AlphaSymbolic':5, 'Integer':6,'Decimal':7, 'Symbolic':8}
         super().__init__(length,'String')
+        self.__type=__types[type]
         self.low=low
         self.high=high
     
-    def generateInt(self):
+    def generateInteger(self):
         arr=[]
         for x in range(self.length):
-            arr.append(random.random(self.low,self.high))
+            arr.append(random.randint(self.low,self.high))
         return arr
 
     def generateAlphaNumeric(self):
@@ -31,32 +32,78 @@ class ArrayGenerator(ClassicGenerator):
         
 
     def generateAlphabet(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            gAN= StringGenerator(rint,'Alphabet')
+            l.append(gAN.generate())
+        return l
 
     def generateAlphabet_Upper(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            gAN= StringGenerator(rint,'Alphabet_Upper')
+            l.append(gAN.generate())
+        return l
+
 
     def generateAlphabet_Lower(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            gAN= StringGenerator(rint,'Alphabet_Lower')
+            l.append(gAN.generate())
+        return l
 
     def generateAlphaSymbolic(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            gAN= StringGenerator(rint,'AlphaSymbolic')
+            l.append(gAN.generate())
+        return l
 
-    def generateInterger(self):
-        pass
+    # def generateInteger(self):
+    #     l=[]
+    #     for i in range(self.length):
+    #         rint=random.randint(self.low,self.high)
+    #         gAN= StringGenerator(rint,'Integer')
+    #         l.append(gAN.generate())
+    #     return l
 
     def generateDecimal(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            num=round(random.random()*10,rint)
+            l.append(num)
+        return l
 
     def generateSymbolic(self):
-        pass
+        l=[]
+        for i in range(self.length):
+            rint=random.randint(self.low,self.high)
+            sym=random.choices(Alphanumerics().symbols(),k=rint)
+            sym=''.join(sym)
+            l.append(sym)
+        return l
 
 
     def generate(self):
         if self.__type == 1:
-            return self.generateInt()
+            return self.generateAlphaNumeric()
         elif self.__type == 2:
-            pass
+            return self.generateAlphabet()
         elif self.__type == 3:
-            pass
-        
+            return self.generateAlphabet_Upper()
+        elif self.__type == 4:
+            return self.generateAlphabet_Lower()
+        elif self.__type == 5:
+            return self.generateAlphaSymbolic()
+        elif self.__type == 6:
+            return self.generateInteger()
+        elif self.__type == 7:
+            return self.generateDecimal()
+        elif self.__type == 8:
+            return self.generateSymbolic()
--- tests/core_test/test_ArrayGenerator.py
+++ tests/core_test/test_ArrayGenerator.py
@@ -3,9 +3,13 @@ import random
 from core.ArrayGenerator import ArrayGenerator 
 
 class ArrayGeneratorTestMethods(unittest.TestCase):
-    def test_AlphaNumericGenerator(self):
-        length=random.randint(1,100)
-        high=random.randint(50,100)
-        low=random.randint(1,50)
-        argen=ArrayGenerator(length,'AlphaNumeric',low,high)
-        print(argen.generateAlphaNumeric())
+    def test_AllArrayGenerators(self):
+        types={'AlphaNumeric':1,'Alphabet':2,'Alphabet_Upper':3,'Alphabet_Lower':4, 'AlphaSymbolic':5, 'Integer':6,'Decimal':7, 'Symbolic':8}
+        for x in types:
+            length=random.randint(1,100)
+            high=random.randint(50,100)
+            low=random.randint(1,50)
+            argen=ArrayGenerator(length,x,low,high)
+            print(x)
+            print(argen.generate())
+            print()
